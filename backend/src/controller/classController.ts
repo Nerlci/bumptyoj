e360298e8becaae25a1d51fa99294184b6375101
weiguang1 <weiguangwzh@qq.com>
2024-05-08 21:37:13 +0800

    chore: Implement classController with create, get, modify, and delete methods

---
+++ backend/src/controller/classController.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from "express";
+import { Class, class_, responseBase } from "../schema";
+import { classService } from "../service/classService";
+
+const createClass = async (req: Request, res: Response) => {
+  const { ...data } = req.body;
+  const classBody = class_.parse(data);
+
+  const result = await classService.createClass(classBody);
+
+  res.send(
+    responseBase.parse({
+      code: "200",
+      payload: result,
+      error: {
+        msg: "",
+      },
+    }),
+  );
+};
+
+const getClass = async (req: Request, res: Response) => {
+  const classId = Number(req.query.classId);
+  const result = await classService.getClass(classId);
+
+  res.send(
+    responseBase.parse({
+      code: "200",
+      payload: result,
+      error: {
+        msg: "",
+      },
+    }),
+  );
+};
+
+const modifyClass = async (req: Request, res: Response) => {
+  const { ...data } = req.body;
+  const classBody = class_.parse(data);
+
+  const result = await classService.modifyClass(
+    Number(req.query.classId),
+    classBody,
+  );
+
+  res.send(
+    responseBase.parse({
+      code: "200",
+      payload: result,
+      error: {
+        msg: "",
+      },
+    }),
+  );
+};
+
+const deleteClass = async (req: Request, res: Response) => {
+  const classId = Number(req.query.classId);
+  const result = await classService.deleteClass(classId);
+
+  res.send(
+    responseBase.parse({
+      code: "200",
+      payload: result,
+      error: {
+        msg: "",
+      },
+    }),
+  );
+};
+
+export const classController = {
+  createClass,
+  getClass,
+  modifyClass,
+  deleteClass,
+};
