154fa5e1786878c62185e6572a58bf6f5f2dc133
Nerlci_ <nerlci.underline@outlook.com>
2024-05-08 19:17:40 +0800

    implement testdata api

--- backend/src/controller/problemController.ts
+++ backend/src/controller/problemController.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from "express";
+
+import { unlink } from "fs";
+
 import { problemService } from "../service/problemService";
-import { encryptPassword, validatePassword } from "../utils/utils";
-import { problem, problemMetadata, responseBase } from "../schema";
+import { problem, responseBase } from "../schema";
+import test from "node:test";
+import { count } from "console";
+
+interface TestdataRequest extends Request {
+  files: {
+    inputFile: Express.Multer.File[];
+    outputFile: Express.Multer.File[];
+  };
+}
 
 const createProblem = async (req: Request, res: Response) => {
   const { metadata, ...rest } = req.body;
@@ -133,6 +144,170 @@ const listProblem = async (req: Request, res: Response) => {
   );
 };
 
+const deletePreviousFiles = async (testdataId: number) => {
+  const previous = await problemService.getTestdata(testdataId);
+
+  if (!previous) {
+    return false;
+  }
+
+  unlink(previous.input, () => {});
+  unlink(previous.output, () => {});
+
+  return true;
+};
+
+const getTestdata = async (req: Request, res: Response) => {
+  const problemId = Number(req.query.problemId);
+  const result = await problemService.getTestdataByProblemId(problemId);
+
+  const resTestdata = result.map((result) => {
+    return {
+      testdataId: result.id,
+      inputFilename: result.inputFilename,
+      outputFilename: result.outputFilename,
+    };
+  });
+
+  res.send(
+    responseBase.parse({
+      code: "200",
+      payload: {
+        count: resTestdata.length,
+        problemId,
+        testdata: resTestdata,
+      },
+      error: {
+        msg: "",
+      },
+    }),
+  );
+};
+
+const createTestdata = async (req: Request, res: Response) => {
+  const problemId = Number(req.body.problemId);
+
+  const filesReq = req as TestdataRequest;
+  const input = filesReq.files!.inputFile[0].path;
+  const output = filesReq.files!.outputFile[0].path;
+  const inputFilename = filesReq.files!.inputFile[0].originalname;
+  const outputFilename = filesReq.files!.outputFile[0].originalname;
+
+  const result = await problemService.createTestdata(
+    problemId,
+    input,
+    output,
+    inputFilename,
+    outputFilename,
+  );
+
+  res.send(
+    responseBase.parse({
+      code: "200",
+      payload: {
+        problemId: result.problemId,
+        testdata: {
+          testdataId: result.id,
+          inputFilename: result.inputFilename,
+          outputFilename: result.outputFilename,
+        },
+      },
+      error: {
+        msg: "",
+      },
+    }),
+  );
+};
+
+const modifyTestdata = async (req: Request, res: Response) => {
+  const testdataId = Number(req.query.testdataId);
+
+  const filesReq = req as TestdataRequest;
+  const input = filesReq.files!.inputFile[0].path;
+  const output = filesReq.files!.outputFile[0].path;
+  const inputFilename = filesReq.files!.inputFile[0].originalname;
+  const outputFilename = filesReq.files!.outputFile[0].originalname;
+
+  if (!(await deletePreviousFiles(testdataId))) {
+    res.send(
+      responseBase.parse({
+        code: "400",
+        payload: {},
+        error: {
+          msg: "Testdata not found",
+        },
+      }),
+    );
+    return;
+  }
+
+  const result = await problemService.modifyTestdata(
+    testdataId,
+    input,
+    output,
+    inputFilename,
+    outputFilename,
+  );
+
+  res.send(
+    responseBase.parse({
+      code: "200",
+      payload: {
+        problemId: result.problemId,
+        testdata: {
+          testdataId: result.id,
+          inputFilename: result.inputFilename,
+          outputFilename: result.outputFilename,
+        },
+      },
+      error: {
+        msg: "",
+      },
+    }),
+  );
+};
+
+const deleteTestdata = async (req: Request, res: Response) => {
+  const testdataId = Number(req.query.testdataId);
+
+  if (!(await deletePreviousFiles(testdataId))) {
+    res.send(
+      responseBase.parse({
+        code: "400",
+        payload: {},
+        error: {
+          msg: "Testdata not found",
+        },
+      }),
+    );
+    return;
+  }
+
+  const result = await problemService.deleteTestdata(testdataId);
+  res.send(
+    responseBase.parse({
+      code: "200",
+      payload: {},
+      error: {
+        msg: "",
+      },
+    }),
+  );
+};
+
+const downloadTestdata = async (req: Request, res: Response) => {
+  // TODO: Handle files
+  const testdataId = Number(req.query.testdataId);
+  const file = Number(req.query.file);
+  const result = await problemService.getTestdata(testdataId);
+
+  if (file === 0) {
+    res.download(result!.input, result!.inputFilename);
+  } else {
+    res.download(result!.output, result!.outputFilename);
+  }
+};
+
 const problemController = {
   createProblem,
   modifyProblem,
@@ -140,6 +315,11 @@ const problemController = {
   deleteProblem,
   countProblem,
   listProblem,
+  getTestdata,
+  createTestdata,
+  modifyTestdata,
+  deleteTestdata,
+  downloadTestdata,
 };
 
 export { problemController };
