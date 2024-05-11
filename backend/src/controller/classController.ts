6567c3572e3ca13ee6bb1f1f2765b9ddcb25be56
weiguang1 <weiguangwzh@qq.com>
2024-05-11 11:31:59 +0800

    refactor: Add error handling to classController endpoints

--- backend/src/controller/classController.ts
+++ backend/src/controller/classController.ts
@@ -1,71 +1,88 @@
 import { Request, Response } from "express";
 import { Class, class_, responseBase } from "../schema";
 import { classService } from "../service/classService";
+import { handleErrors } from "../utils/utils";
 
 const createClass = async (req: Request, res: Response) => {
   const { ...data } = req.body;
-  const classBody = class_.parse(data);
+  try {
+    const classBody = class_.parse(data);
 
-  const result = await classService.createClass(classBody);
+    const result = await classService.createClass(classBody);
 
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: result,
-      error: {
-        msg: "",
-      },
-    }),
-  );
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: result,
+        error: {
+          msg: "",
+        },
+      }),
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const getClass = async (req: Request, res: Response) => {
   const classId = Number(req.query.classId);
-  const result = await classService.getClass(classId);
+  try {
+    const result = await classService.getClass(classId);
 
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: result,
-      error: {
-        msg: "",
-      },
-    }),
-  );
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: result,
+        error: {
+          msg: "",
+        },
+      }),
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const modifyClass = async (req: Request, res: Response) => {
   const { ...data } = req.body;
   data.teacherId = Number(data.teacherId);
   data.classId = Number(req.query.classId);
-  const classBody = class_.parse(data);
 
-  const result = await classService.modifyClass(classBody.classId, classBody);
+  try {
+    const classBody = class_.parse(data);
+    const result = await classService.modifyClass(classBody.classId, classBody);
 
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: result,
-      error: {
-        msg: "",
-      },
-    }),
-  );
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: result,
+        error: {
+          msg: "",
+        },
+      }),
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const deleteClass = async (req: Request, res: Response) => {
   const classId = Number(req.query.classId);
-  const result = await classService.deleteClass(classId);
+  try {
+    const result = await classService.deleteClass(classId);
 
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: {},
-      error: {
-        msg: "",
-      },
-    }),
-  );
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: {},
+        error: {
+          msg: "",
+        },
+      }),
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const listClass = async (req: Request, res: Response) => {
@@ -73,17 +90,21 @@ const listClass = async (req: Request, res: Response) => {
   const count = Number(req.query.count);
   const offset = Number(req.query.offset);
 
-  const result = await classService.listClass(userId, count, offset);
+  try {
+    const result = await classService.listClass(userId, count, offset);
 
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: { classes: result },
-      error: {
-        msg: "",
-      },
-    }),
-  );
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: { classes: result },
+        error: {
+          msg: "",
+        },
+      }),
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const countClass = async (req: Request, res: Response) => {
