b65321cc5d7ddf27019f4f277c49fc9d0317a45b
Nerlci_ <nerlci.underline@outlook.com>
2024-05-11 22:43:32 +0800

    do not show state button in contest detail when not logged in

--- frontend/src/pages/Contest/contestDetail.vue
+++ frontend/src/pages/Contest/contestDetail.vue
@@ -11,7 +11,10 @@
       :format="() => ''"
     ></el-progress>
 
-    <span v-if="contest.type == 0" class="state-button">
+    <span
+      v-if="contest.type == 0 && this.$store.state.status.isLogin"
+      class="state-button"
+    >
       <el-button disabled type="info" v-if="checkTime(contest.endTime)">
         已结束
       </el-button>
