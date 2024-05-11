a1c24b4a47b568960fdca39e819fd47f1c16ead2
weiguang1 <weiguangwzh@qq.com>
2024-05-11 12:24:05 +0800

    refactor: Add error handling to submissionController endpoints

--- backend/src/controller/submissionController.ts
+++ backend/src/controller/submissionController.ts
@@ -1,60 +1,69 @@
 import { Request, Response } from "express";
 import { submissionService } from "../service/submissionService";
 import { responseBase, submission } from "../schema";
+import { handleErrors } from "../utils/utils";
 
 const postSubmit = async (req: Request, res: Response) => {
   const { problemId, problemsetId, code, language } = req.body;
   const userId = res.locals.user.userId;
 
-  const submissionData = submission.parse({
-    problemId,
-    problemsetId,
-    userId,
-    code,
-    language,
-    length: Buffer.byteLength(code, "utf-8"),
-  });
+  try {
+    const submissionData = submission.parse({
+      problemId,
+      problemsetId,
+      userId,
+      code,
+      language,
+      length: Buffer.byteLength(code, "utf-8"),
+    });
 
-  const result = await submissionService.handleJudge(submissionData);
+    const result = await submissionService.handleJudge(submissionData);
 
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: {
-        submissionId: result.id,
-        problemId: result.problemId,
-        userId: result.userId,
-        language: result.language,
-        timestamp: result.timestamp,
-      },
-      error: { message: "" },
-    }),
-  );
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: {
+          submissionId: result.id,
+          problemId: result.problemId,
+          userId: result.userId,
+          language: result.language,
+          timestamp: result.timestamp,
+        },
+        error: { message: "" },
+      }),
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const getSubmission = async (req: Request, res: Response) => {
   const submissionId = Number(req.query.submissionId);
 
-  const result = await submissionService.getSubmission(submissionId);
-  const { id, detail, ...rest } = result!;
-  const detailData = detail.map((d) => {
-    const { id, submissionId, ...rest } = d;
-    return {
-      ...rest,
-    };
-  });
-
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: {
-        submissionId: id,
+  try {
+    const result = await submissionService.getSubmission(submissionId);
+    const { id, detail, ...rest } = result!;
+    const detailData = detail.map((d) => {
+      const { id, submissionId, ...rest } = d;
+      return {
         ...rest,
-        detail: detailData,
-      },
-      error: { message: "" },
-    }),
-  );
+      };
+    });
+
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: {
+          submissionId: id,
+          ...rest,
+          detail: detailData,
+        },
+        error: { message: "" },
+      }),
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const listSubmission = async (req: Request, res: Response) => {
@@ -65,50 +74,58 @@ const listSubmission = async (req: Request, res: Response) => {
   const userId = Number(req.query.userId) || undefined;
   const problemId = Number(req.query.problemId) || undefined;
 
-  const result = await submissionService.listSubmission(
-    count,
-    maxId,
-    minId,
-    submissionId,
-    userId,
-    problemId,
-  );
+  try {
+    const result = await submissionService.listSubmission(
+      count,
+      maxId,
+      minId,
+      submissionId,
+      userId,
+      problemId,
+    );
 
-  const resultData = result.map((r) => {
-    const { id, code, ...rest } = r;
-    return {
-      submissionId: id,
-      ...rest,
-    };
-  });
+    const resultData = result.map((r) => {
+      const { id, code, ...rest } = r;
+      return {
+        submissionId: id,
+        ...rest,
+      };
+    });
 
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: {
-        count: result.length,
-        submissions: resultData,
-      },
-      error: { message: "" },
-    }),
-  );
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: {
+          count: result.length,
+          submissions: resultData,
+        },
+        error: { message: "" },
+      }),
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const countSubmission = async (req: Request, res: Response) => {
   const userId = Number(req.query.userId) || undefined;
   const problemId = Number(req.query.problemId) || undefined;
 
-  const result = await submissionService.countSubmission(userId, problemId);
+  try {
+    const result = await submissionService.countSubmission(userId, problemId);
 
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: {
-        count: result,
-      },
-      error: { message: "" },
-    }),
-  );
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: {
+          count: result,
+        },
+        error: { message: "" },
+      }),
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const submissionController = {
