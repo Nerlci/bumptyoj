9529730e2d69429db36525d12bb96eafe6951a1a
weiguang1 <weiguangwzh@qq.com>
2024-05-11 12:23:33 +0800

    refactor: Add error handling to leaderboardController endpoints

--- backend/src/controller/leaderboardController.ts
+++ backend/src/controller/leaderboardController.ts
@@ -1,60 +1,73 @@
 import { Request, Response } from "express";
 import { responseBase } from "../schema";
 import { leaderboardService } from "../service/leaderboardService";
+import { handleErrors } from "../utils/utils";
 
 const countLeaderboard = async (req: Request, res: Response) => {
   const startTime = req.query.startTime as string | undefined;
   const endTime = req.query.endTime as string | undefined;
 
-  const countLeaderboard = await leaderboardService.countLeaderboard(
-    startTime,
-    endTime,
-  );
+  try {
+    const countLeaderboard = await leaderboardService.countLeaderboard(
+      startTime,
+      endTime,
+    );
 
-  res.json(
-    responseBase.parse({
-      code: "200",
-      payload: {
-        leaderboard: countLeaderboard,
-      },
-      error: { msg: "" },
-    }),
-  );
+    res.json(
+      responseBase.parse({
+        code: "200",
+        payload: {
+          leaderboard: countLeaderboard,
+        },
+        error: { msg: "" },
+      }),
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const weightedLeaderboard = async (req: Request, res: Response) => {
   const startTime = req.query.startTime as string | undefined;
   const endTime = req.query.endTime as string | undefined;
 
-  const weightedLeaderboard = await leaderboardService.weightedLeaderboard(
-    startTime,
-    endTime,
-  );
+  try {
+    const weightedLeaderboard = await leaderboardService.weightedLeaderboard(
+      startTime,
+      endTime,
+    );
 
-  res.json(
-    responseBase.parse({
-      code: "200",
-      payload: {
-        leaderboard: weightedLeaderboard,
-      },
-      error: { msg: "" },
-    }),
-  );
+    res.json(
+      responseBase.parse({
+        code: "200",
+        payload: {
+          leaderboard: weightedLeaderboard,
+        },
+        error: { msg: "" },
+      }),
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const problemsetLeaderboard = async (req: Request, res: Response) => {
   const problemsetId = Number(req.query.problemsetId);
 
-  const problemsetLeaderboard =
-    await leaderboardService.problemsetLeaderboard(problemsetId);
+  try {
+    const problemsetLeaderboard =
+      await leaderboardService.problemsetLeaderboard(problemsetId);
 
-  res.json({
-    code: "200",
-    payload: {
-      leaderboard: problemsetLeaderboard,
-    },
-    error: { msg: "" },
-  });
+    res.json({
+      code: "200",
+      payload: {
+        leaderboard: problemsetLeaderboard,
+      },
+      error: { msg: "" },
+    });
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const leaderboardController = {
