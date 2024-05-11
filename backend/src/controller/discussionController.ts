f2095e2bb9ed7e2a066747ebceea66488b0432d5
weiguang1 <weiguangwzh@qq.com>
2024-05-11 12:25:43 +0800

    refactor: Add error handling for discussionController endpoints.

--- backend/src/controller/discussionController.ts
+++ backend/src/controller/discussionController.ts
@@ -1,51 +1,60 @@
 import { Request, Response } from "express";
 import { discussionService } from "../service/discussionService";
 import { comment, discussionPost, responseBase } from "../schema";
+import { handleErrors } from "../utils/utils";
 
 const getPost = async (req: Request, res: Response) => {
   const postId = Number(req.query.postId);
 
-  const result = await discussionService.getPost(postId);
+  try {
+    const result = await discussionService.getPost(postId);
 
-  const resultData = discussionPost.parse({
-    ...result,
-    postId: result!.id,
-    userId: result!.user.id,
-    author: result!.user.username,
-  });
+    const resultData = discussionPost.parse({
+      ...result,
+      postId: result!.id,
+      userId: result!.user.id,
+      author: result!.user.username,
+    });
 
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: resultData,
-      error: { message: "" },
-    }),
-  );
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: resultData,
+        error: { msg: "" },
+      }),
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const createPost = async (req: Request, res: Response) => {
   const userId = Number(res.locals.user.userId);
   const username = String(res.locals.user.username);
 
-  const data = discussionPost.parse({
-    ...req.body,
-    userId,
-    author: username,
-  });
+  try {
+    const data = discussionPost.parse({
+      ...req.body,
+      userId,
+      author: username,
+    });
 
-  const result = await discussionService.createPost(data);
+    const result = await discussionService.createPost(data);
 
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: discussionPost.parse({
-        ...result,
-        postId: result.id,
-        author: username,
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: discussionPost.parse({
+          ...result,
+          postId: result.id,
+          author: username,
+        }),
+        error: { msg: "" },
       }),
-      error: { message: "" },
-    }),
-  );
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const modifyPost = async (req: Request, res: Response) => {
@@ -54,40 +63,48 @@ const modifyPost = async (req: Request, res: Response) => {
   const username = String(res.locals.user.username);
   const postId = Number(req.query.postId);
 
-  const data = discussionPost.parse({
-    ...req.body,
-    userId,
-    author: username,
-  });
+  try {
+    const data = discussionPost.parse({
+      ...req.body,
+      userId,
+      author: username,
+    });
 
-  const result = await discussionService.modifyPost(postId, data);
+    const result = await discussionService.modifyPost(postId, data);
 
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: discussionPost.parse({
-        ...result,
-        postId: result.id,
-        author: username,
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: discussionPost.parse({
+          ...result,
+          postId: result.id,
+          author: username,
+        }),
+        error: { msg: "" },
       }),
-      error: { message: "" },
-    }),
-  );
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const deletePost = async (req: Request, res: Response) => {
   // TODO: check if the post is created by current user
   const postId = Number(req.query.postId);
 
-  const result = await discussionService.deletePost(postId);
+  try {
+    const result = await discussionService.deletePost(postId);
 
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: {},
-      error: { message: "" },
-    }),
-  );
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: {},
+        error: { msg: "" },
+      }),
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const listPost = async (req: Request, res: Response) => {
@@ -95,30 +112,34 @@ const listPost = async (req: Request, res: Response) => {
   const offset = Number(req.query.offset);
   const count = Number(req.query.count);
 
-  const result = await discussionService.listPost(category, offset, count);
+  try {
+    const result = await discussionService.listPost(category, offset, count);
 
-  const resultData = result.map((post) =>
-    discussionPost.parse({
-      ...post,
-      postId: post.id,
-      userId: post.user.id,
-      author: post.user.username,
-    }),
-  );
+    const resultData = result.map((post) =>
+      discussionPost.parse({
+        ...post,
+        postId: post.id,
+        userId: post.user.id,
+        author: post.user.username,
+      }),
+    );
 
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: {
-        posts: resultData.map((post) => {
-          const { content, ...rest } = post;
-          return rest;
-        }),
-        count: resultData.length,
-      },
-      error: { message: "" },
-    }),
-  );
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: {
+          posts: resultData.map((post) => {
+            const { content, ...rest } = post;
+            return rest;
+          }),
+          count: resultData.length,
+        },
+        error: { msg: "" },
+      }),
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const countPost = async (req: Request, res: Response) => {
@@ -128,7 +149,7 @@ const countPost = async (req: Request, res: Response) => {
     responseBase.parse({
       code: "200",
       payload: { count: result },
-      error: { message: "" },
+      error: { msg: "" },
     }),
   );
 };
@@ -138,48 +159,56 @@ const getComment = async (req: Request, res: Response) => {
   const offset = Number(req.query.offset);
   const count = Number(req.query.count);
 
-  const result = await discussionService.getComment(postId, offset, count);
+  try {
+    const result = await discussionService.getComment(postId, offset, count);
 
-  const resultData = result.map((c) =>
-    comment.parse({
-      ...c,
-      commentId: c.id,
-      userId: c.user.id,
-      author: c.user.username,
-    }),
-  );
+    const resultData = result.map((c) =>
+      comment.parse({
+        ...c,
+        commentId: c.id,
+        userId: c.user.id,
+        author: c.user.username,
+      }),
+    );
 
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: { postId, comments: resultData, count: resultData.length },
-      error: { message: "" },
-    }),
-  );
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: { postId, comments: resultData, count: resultData.length },
+        error: { msg: "" },
+      }),
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const createComment = async (req: Request, res: Response) => {
   const userId = Number(res.locals.user.userId);
   const username = String(res.locals.user.username);
-  const data = comment.parse({
-    ...req.body,
-    userId,
-    author: username,
-  });
+  try {
+    const data = comment.parse({
+      ...req.body,
+      userId,
+      author: username,
+    });
 
-  const result = await discussionService.createComment(data);
+    const result = await discussionService.createComment(data);
 
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: comment.parse({
-        ...result,
-        commentId: result.id,
-        author: username,
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: comment.parse({
+          ...result,
+          commentId: result.id,
+          author: username,
+        }),
+        error: { msg: "" },
       }),
-      error: { message: "" },
-    }),
-  );
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const modifyComment = async (req: Request, res: Response) => {
@@ -187,40 +216,48 @@ const modifyComment = async (req: Request, res: Response) => {
   const userId = Number(res.locals.user.userId);
   const username = String(res.locals.user.username);
   const commentId = Number(req.query.commentId);
-  const data = comment.parse({
-    ...req.body,
-    userId,
-    author: username,
-  });
+  try {
+    const data = comment.parse({
+      ...req.body,
+      userId,
+      author: username,
+    });
 
-  const result = await discussionService.modifyComment(commentId, data);
+    const result = await discussionService.modifyComment(commentId, data);
 
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: comment.parse({
-        ...result,
-        commentId: result.id,
-        author: username,
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: comment.parse({
+          ...result,
+          commentId: result.id,
+          author: username,
+        }),
+        error: { msg: "" },
       }),
-      error: { message: "" },
-    }),
-  );
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const deleteComment = async (req: Request, res: Response) => {
   // TODO: check if the comment is created by current user
   const commentId = Number(req.query.commentId);
 
-  const result = await discussionService.deleteComment(commentId);
+  try {
+    const result = await discussionService.deleteComment(commentId);
 
-  res.send(
-    responseBase.parse({
-      code: "200",
-      payload: {},
-      error: { message: "" },
-    }),
-  );
+    res.send(
+      responseBase.parse({
+        code: "200",
+        payload: {},
+        error: { msg: "" },
+      }),
+    );
+  } catch (error) {
+    handleErrors(error, res);
+  }
 };
 
 const discussionController = {
